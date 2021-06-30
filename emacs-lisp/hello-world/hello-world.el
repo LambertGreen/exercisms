0244ba2e991f66e00c1a23f8a6877e5b44e7be28
Lambert Green <lgreen@lgreen-mac.hsd1.wa.comcast.net>
2021-06-30 12:56:25 -0700

    Adds elisp hello-world exercise

---
+++ emacs-lisp/hello-world/hello-world.el
@@ -0,0 +1,13 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun hello ()
+  "Say hello to the world."
+    "Hello, World!"
+    )
+
+(provide 'hello-world)
+;;; hello-world.el ends here
