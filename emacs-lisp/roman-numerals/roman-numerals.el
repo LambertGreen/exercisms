068492b8a883250bf7c1e85ea8cda789d576d2fc
Lambert Green <lgreen@lgreen-mac.hsd1.wa.comcast.net>
2021-07-01 13:26:46 -0700

    Add start of elisp exercism 'roman-numerals'

---
+++ emacs-lisp/roman-numerals/roman-numerals.el
@@ -0,0 +1,12 @@
+;;; roman-numerals.el --- roman-numerals Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun to-roman (number)
+  "Return the roman numeral for NUMBER."
+  )
+
+(provide 'roman-numerals)
+;;; roman-numerals.el ends here
