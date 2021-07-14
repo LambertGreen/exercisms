e4395de64a5e99b58138dd3e26ee64be7d30e0cb
Lambert Green <lambert_green@msn.com>
2021-07-13 17:36:39 -0700

    Adds solution to exercise 'Word Count'

--- emacs-lisp/word-count/word-count.el
+++ emacs-lisp/word-count/word-count.el
@@ -4,6 +4,35 @@
 
 ;;; Code:
 
+(defun inc-word-count (word word-frequencies)
+  "Increment the frequency of WORD in WORD-FREQUENCIES."
+  (puthash word (1+ (gethash word word-frequencies 0)) word-frequencies))
+
+(defun hash-to-alist (hash-map)
+  "Return HASH-MAP as an alist."
+  (let ((alist ()))
+    (maphash
+     (lambda (k v)
+       (push (cons k v) alist))
+     hash-map)
+    alist))
+
+(defun normalize (word)
+  "Return WORD as trimmed, lowercase and without punctuation."
+  (let ((normalized (downcase (string-trim word))))
+    (replace-regexp-in-string "\[!@$%^&\]" "" normalized)))
+
+(defun tokenize (phrase)
+  "Return a list of words from PHRASE split by common delimiters."
+     (delete "" (split-string phrase "\[\s;:,\]")))
+
+(defun word-count (phrase)
+  "Return the frequncy of words in a PHRASE."
+  (let ((word-frequencies (make-hash-table :test 'equal)))
+    (mapc
+     (lambda (word) (inc-word-count (normalize word) word-frequencies))
+     (tokenize phrase))
+    (hash-to-alist word-frequencies)))
 
 (provide 'word-count)
 ;;; word-count.el ends here
