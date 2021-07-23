30ac0fee202544441df1da1fc4c7ea27af7295ce
Lambert Green <lambert_green@msn.com>
2021-07-23 16:22:53 -0700

    Solution to elisp exercise difference-of-squares

--- emacs-lisp/difference-of-squares/difference-of-squares.el
+++ emacs-lisp/difference-of-squares/difference-of-squares.el
@@ -4,11 +4,21 @@
 
 ;;; Code:
 
-(defun square-of-sum (x)
-  x)
+(defun square (n)
+  "Return N x N."
+  (* n n))
 
-(defun square-of-sum (x)
-  x)
+(defun square-of-sum (num)
+  "Return the square of the sum of numbers from 1 to NUM."
+  (square (apply '+ (number-sequence 1 num 1))))
+
+(defun sum-of-squares (num)
+  "Return the sum of the squares of numbers from 1 to NUM."
+  (apply '+ (mapcar 'square (number-sequence 1 num 1))))
+
+(defun difference (num)
+  "Return the difference between the square-of-sum of NUM and the sum-of-squares of NUM."
+  (- (square-of-sum num) (sum-of-squares num)))
 
 (provide 'difference-of-squares)
 ;;; difference-of-squares.el ends here
