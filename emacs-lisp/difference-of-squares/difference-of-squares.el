54ffa876bae4e89a37321c1d4332bbefd51d429d
Lambert Green <lambert_green@msn.com>
2021-07-23 16:21:47 -0700

    Adds start of elisp exercise, difference-of-squares

---
+++ emacs-lisp/difference-of-squares/difference-of-squares.el
@@ -0,0 +1,14 @@
+;;; difference-of-squares.el --- Difference of Squares (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun square-of-sum (x)
+  x)
+
+(defun square-of-sum (x)
+  x)
+
+(provide 'difference-of-squares)
+;;; difference-of-squares.el ends here
